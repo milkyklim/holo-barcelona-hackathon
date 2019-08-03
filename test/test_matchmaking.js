dd291732af6ede8abaee3215620fc435b6a84345
Igor Line <linesethenoiz@gmail.com>
2019-08-03 17:31:46 +0200

    Refactor tests

--- test/test_matchmaking.js
+++ test/test_matchmaking.js
@@ -1,47 +1,48 @@
 const {
-  results,
-  lastResult,
-  addressLength,
-  tabLength,
   createTradeProposal,
   getTradeProposals,
   acceptTradeProposal,
 } = require('./utils');
 
-const checkTradeProposalAccepted = async (
-  t,
-  agent,
-  agentName,
-  itemsNumber,
-  discardedAddress,
-) => {
-  // check that agent sees only itemsNumber - 1 trades now
-  const totalTradeProposals = await getTradeProposals(agent);
-  t.equal(
-    totalTradeProposals.Ok.length,
-    itemsNumber - 1,
-    `${agentName} can see ${itemsNumber - 1} proposal(s)`,
-  );
-  t.equal(
-    totalTradeProposals.Ok[0].address,
-    discardedAddress,
-    `${agentName} sees the correct discarded option`,
-  );
+const createTradeProposals = async (items, seller) => {
+  for (const [item, desc] of Object.entries(items)) {
+    await createTradeProposal(seller, item, desc);
+  }
 };
 
 module.exports = scenario => {
-  scenario('Alice can create trade proposal', async (s, t, { alice }) => {
-    await createTradeProposal(alice, 'Socks', 'Old and smelly');
+  scenario('Alice can create trade proposals', async (s, t, { alice }) => {
+    let tradeProposals;
+    tradeProposals = (await getTradeProposals(alice)).Ok;
+    t.equal(tradeProposals.length, 0, 'There are no trade proposals available');
+    let proposalAddress = await createTradeProposal(
+      alice,
+      'Socks',
+      'Old and smelly',
+    );
     t.equal(
-      lastResult().Ok.length,
-      addressLength,
+      proposalAddress.Ok,
+      'QmZFbXGzFWpBPXSh2jZMRYq41VJuQ7rt5f6dD8MFSHAeck',
       'Alice created proposal successfully',
     );
-    console.log(JSON.stringify(lastResult(), null, tabLength));
+
+    tradeProposals = (await getTradeProposals(alice)).Ok;
+    t.equal(
+      tradeProposals.length,
+      1,
+      'Alice get correct number of trade proposals',
+    );
+    proposalAddress = await createTradeProposal(alice, 'Apple', 'Eatable');
+    tradeProposals = (await getTradeProposals(alice)).Ok;
+    t.equal(
+      tradeProposals.length,
+      2,
+      'Alice get correct number of trade proposals',
+    );
   });
 
   scenario(
-    'Alice creates multiple trade proposals and Bob gets the correct number of them',
+    'Bob gets the correct number of created proposals',
     async (s, t, { alice, bob }) => {
       // create 5 items for proposals
       const items = {
@@ -54,32 +55,20 @@ module.exports = scenario => {
 
       const itemsNumber = Object.keys(items).length;
 
-      for (const [item, desc] of Object.entries(items)) {
-        await createTradeProposal(alice, item, desc);
-        t.equal(
-          lastResult().Ok.length,
-          addressLength,
-          `Alice created ${item} proposal successfully`,
-        );
-      }
+      await createTradeProposals(items, alice);
 
       // let Bob get the total number of entries
-      await getTradeProposals(bob);
+      const { Ok: tradeProposals } = await getTradeProposals(bob);
       t.equal(
-        lastResult().Ok.length,
+        tradeProposals.length,
         itemsNumber,
         `Bob can see all ${itemsNumber} proposals`,
       );
-
-      // print all results
-      results.forEach((result, i) => {
-        console.log(`${i}: ${JSON.stringify(result, null, tabLength)}\n`);
-      });
     },
   );
 
   scenario(
-    'Alice creates 2 trade proposals and Bob accepts one trade proposal',
+    'Bob can accept Alice trade proposal',
     async (s, t, { alice, bob }) => {
       // create 2 items for proposals
       const items = {
@@ -89,55 +78,46 @@ module.exports = scenario => {
 
       const itemsNumber = Object.keys(items).length;
 
-      for (const [item, desc] of Object.entries(items)) {
-        await createTradeProposal(alice, item, desc);
-        t.equal(
-          lastResult().Ok.length,
-          addressLength,
-          `Alice created ${item} proposal successfully`,
-        );
-      }
+      await createTradeProposals(items, alice);
 
-      await getTradeProposals(bob);
-      t.equal(
-        lastResult().Ok.length,
-        itemsNumber,
-        `Bob can see ${itemsNumber} proposals`,
-      );
-      console.log(JSON.stringify(lastResult(), null, tabLength));
+      const { Ok: tradeProposals } = await getTradeProposals(bob);
 
-      let bobDiscardedAddress = lastResult().Ok[0].address;
-      let bobChoiceAddress = lastResult().Ok[1].address;
+      let bobDiscardedAddress = tradeProposals[0].address;
+      let bobChoiceAddress = tradeProposals[1].address;
 
       // uses default created_at
       await acceptTradeProposal(bob, bobChoiceAddress);
-      // check that alice sees only itemsNumber - 1 trades now
-      await checkTradeProposalAccepted(
-        t,
-        alice,
-        'Alice',
-        itemsNumber,
-        bobDiscardedAddress,
+      // check that alice sees only proper trade now
+      const { Ok: aliceTotalTradeProposals } = await getTradeProposals(alice);
+      t.equal(
+        aliceTotalTradeProposals.length,
+        itemsNumber - 1,
+        `Alice can see ${itemsNumber - 1} proposal(s)`,
       );
-      // check that bob sees only itemsNumber - 1 trades now
-      await checkTradeProposalAccepted(
-        t,
-        bob,
-        'Bob',
-        itemsNumber,
+      t.equal(
+        aliceTotalTradeProposals[0].address,
         bobDiscardedAddress,
+        `Alice sees the correct discarded option`,
       );
-
-      results.forEach((result, i) => {
-        console.log(`${i}: ${JSON.stringify(result, null, tabLength)}\n`);
-      });
+      // check that bob sees only proper trade now
+      const { Ok: bobTotalTradeProposals } = await getTradeProposals(alice);
+      t.equal(
+        bobTotalTradeProposals.length,
+        itemsNumber - 1,
+        `Bob can see ${itemsNumber - 1} proposal(s)`,
+      );
+      t.equal(
+        bobTotalTradeProposals[0].address,
+        bobDiscardedAddress,
+        `Bob sees the correct discarded option`,
+      );
     },
   );
 
-  scenario(
-    'Alice creates a trade proposal and tries to accept it herself',
-    async (s, t, { alice, bob }) => {
-      // TODO:
-    },
-  );
+  // scenario(
+  // 'Alice creates a trade proposal and tries to accept it herself',
+  // async () => {
+  // // TODO
+  // },
+  // );
 };
